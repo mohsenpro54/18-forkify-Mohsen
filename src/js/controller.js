6f38590a7a89c18eb5aa86ade67fb80c8eb2e8cc
bedhief <mohsengafsi123@hotmail.com>
2021-12-24 18:40:39 +0100

    add some modification

--- src/js/controller.js
+++ src/js/controller.js
@@ -112,10 +112,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('welcome to the application');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -124,6 +120,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
